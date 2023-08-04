d7589c85f6f5d1a9f737626f608d6c99e6e0ae6b
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-04 17:40:01 +0530

    operator

---
+++ script-003.js
@@ -0,0 +1,48 @@
+// In JavaScript, an operator is a special symbol used to perform operations on operands (values and variables).
+
+//  - Arithmetic Operators
+//   - + Addition
+//   - - Subtraction
+//   - * Multiplication
+//   - / Division
+//   - % Remainder
+//   - ++ increment (by 1)
+//   - -- decrement (by 1)
+//   - ** exponentiation (Power)
+
+// - Assignment Operators
+//   - = Assignment
+//   - += Addition Assignment
+//   - -= Subtraction Assignment
+//   - *= Multiplication Assignment
+//   - /= Division Assignment
+//   - %= Remainder Assignment
+//   - **= exponentiation (Power) Assignment
+
+// - Comparison Operators: Comparison operators compare two values and return a boolean value, either true or false
+//   -  == : (Equal to) return true if the opereands are equal.
+//   -  != : (Not equal to) return true if the opereands are not equal.
+//   - === : (Strict equal to) return true if the opereands are equal and of the same type.
+//   - !== : (Strict not equal to) return true if the opereands are equal but of different type or not equal at all.
+//   -   > : (Greater than) return true if left opereand is greater than the right operand.
+//   -  >= : (Greater than or equal to) return true if left opereand is greater than or equal to the right operand.
+//   -   < : (Less than) return true if left opereand is less than the right operand.
+//   -  <= : (Greater than or equal to) return true if left opereand is less than or equal to the right operand.
+
+// - Logial Operators
+// - Bitwise Operators
+// - String Operators 
+
+console.log(1 + 2);
+console.log(1 - 2);
+console.log(1 * 2);
+console.log(1 / 2);
+console.log(1 % 2);
+let a = 1;
+console.log(a++);
+console.log(a--);
+console.log(1 ** 2);
+
+console.log('Assignment Operator - ');
+a = 5;
+console.log('a = 5:', a);
