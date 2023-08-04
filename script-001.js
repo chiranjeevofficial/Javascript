c8b8e08dffaa93ef7029d2dddb3b5ba239b53698
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-04 14:07:36 +0530

    introduction to javascript

---
+++ script-001.js
@@ -0,0 +1,11 @@
+// Introduction to Javascript
+
+console.log("Namaste Javascript");
+
+let a = 12;
+let b = 3;
+
+const name = "Chiranjeev Kashyap";
+console.log("a+b:",a+b);
+
+console.log("Name: ",name);
