508018ae9e50a6f6c55efdf2a0516f21be85dd0f
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-05 19:58:09 +0530

    iteration statement

---
+++ script-006.js
@@ -0,0 +1,19 @@
+for(let i = 0 ; i < 10 ; i++) {
+    if (i == 6) {
+        console.log('i == 6');
+        break;
+    }
+    console.log('Namaste Java', i);
+}
+
+let index = 0;
+
+while (index < 5) {
+    console.log('Namaste Javascript',index);
+    index++;
+}
+
+do{
+    console.log('Namast Coder'); 
+    index++;
+} while (index < 10);
