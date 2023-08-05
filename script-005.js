4d55628a130d2d055c90ece680c80161f4266108
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-05 19:42:06 +0530

    switch case

---
+++ script-005.js
@@ -0,0 +1,16 @@
+const grade = 'A';
+
+switch (grade) {
+    case 'A':
+        console.log('Very Good Grade');
+        break;
+    case 'B':
+        console.log('Good Grade');
+        break;
+    case 'C':
+        console.log('Average Grade');
+        break;
+    default:
+        console.log('Low Grade');
+        break;
+}
